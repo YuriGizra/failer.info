1dbfe88f2d1bc2f493998cc0cc898a3f1daffe14
Yuri Failer <yuri.failer@gmail.com>
2014-09-15 22:38:05 +0300

    1

--- Gruntfile.js
+++ Gruntfile.js
@@ -291,7 +291,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: 'git@github.com:YuriGizra/YuriGizra.github.io.git',
+          remote: 'git@github.com:YuriGizra.git',
           branch: 'gh-pages',
           commit: true,
           push: true
